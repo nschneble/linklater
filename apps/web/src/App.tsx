745ef33f8da72c07d37704966d65614a8754862b
Nick Schneble <nschneble@users.noreply.github.com>
2025-12-10 12:06:11 -0500

    scaffold Vite + React + Tailwind

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-emerald-400">Linklater</h1>
+      <p className="italic">A del.icio.us reinterpretation of Instapaper.</p>
+    </>
+  )
+}
+
+export default App;
