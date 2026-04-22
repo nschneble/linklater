e30d5327d00f34f0db7bb28b06399968f33da1ce
Nick Schneble <nschneble@users.noreply.github.com>
2026-04-21 23:10:11 -0400

    apply formatting

--- apps/api/src/metadata/metadata.service.ts
+++ apps/api/src/metadata/metadata.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { MAX_DESCRIPTION_LENGTH, MAX_URL_LENGTH } from './metadata.constants.js';
+import {
+  MAX_DESCRIPTION_LENGTH,
+  MAX_URL_LENGTH,
+} from './metadata.constants.js';
 import { PrismaService } from '@linklater/prisma';
 import { QueueService, QUEUES } from '@linklater/queue';
 import * as cheerio from 'cheerio';
